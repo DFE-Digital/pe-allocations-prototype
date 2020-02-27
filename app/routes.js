c67db1db008d6599ed6c2a198f4213c670f8d413
Jesse Yuen <hello@jesseyuen.com>
2020-02-27 16:59:55 +0000

    Initial structure added

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,18 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+// Branching
+router.post('/pe-allocation/step-1', function (req, res) {
+    // Get the answer from session data
+    // The name between the quotes is the same as the 'name' attribute on the input elements
+    // However in JavaScript we can't use hyphens in variable names
+
+    let firstTime = req.session.data['first-time']
+
+    if (firstTime === 'No ') {
+        res.redirect('/pe-allocation/thanks')
+    } else {
+        res.redirect('/pe-allocation/step-2')
+    }
+})
