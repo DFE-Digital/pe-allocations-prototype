95a3cb15f8c3fd0eb18cb85a42e17b39168f86b1
Jesse Yuen <hello@jesseyuen.com>
2020-03-04 11:27:33 +0000

    Fix up the flow

--- app/routes.js
+++ app/routes.js
@@ -2,35 +2,33 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
-
-
 // Branching
-router.post('/pe-allocation/step-2', function (req, res) {
+router.post('/pe-allocation/step-1', function (req, res) {
     // Get the answer from session data
     // The name between the quotes is the same as the 'name' attribute on the input elements
     // However in JavaScript we can't use hyphens in variable names
     
     let firstTime = req.session.data['first-time']
     
-    if (firstTime === 'No') {
+    if (firstTime === 'Yes') {
         res.redirect('/pe-allocation/thanks')
     } else {
-        res.redirect('/pe-allocation/step-2')
+        res.redirect('/pe-allocation/request-places')
     }
 })
 
-router.post('/pe-allocation/another-course', function (req, res) {
-    // Get the answer from session data
-    // The name between the quotes is the same as the 'name' attribute on the input elements
-    // However in JavaScript we can't use hyphens in variable names
+// router.post('/pe-allocation/another-course', function (req, res) {
+//     // Get the answer from session data
+//     // The name between the quotes is the same as the 'name' attribute on the input elements
+//     // However in JavaScript we can't use hyphens in variable names
     
-    let firstTime = req.session.data['another-course']
+//     let firstTime = req.session.data['another-course']
     
-    if (firstTime === 'Yes') {
-        res.redirect('/pe-allocation/next-entry')
-    } else {
-        res.redirect('/pe-allocation/submit-answers')
-    }
-})
+//     if (firstTime === 'Yes') {
+//         res.redirect('/pe-allocation/next-entry')
+//     } else {
+//         res.redirect('/pe-allocation/submit-answers')
+//     }
+// })
 
 module.exports = router
