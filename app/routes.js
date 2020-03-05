3662ce9a91444ce5e5e77ce43755352dabd1ec44
Jesse Yuen <hello@jesseyuen.com>
2020-03-05 14:19:34 +0000

    path tweaks

--- app/routes.js
+++ app/routes.js
@@ -31,6 +31,20 @@ router.post('/pe-allocation/offer-pe-2', function (req, res) {
     }
 })
 
+router.post('/pe-allocation/offer-pe-3', function (req, res) {
+    // Get the answer from session data
+    // The name between the quotes is the same as the 'name' attribute on the input elements
+    // However in JavaScript we can't use hyphens in variable names
+    
+    let firstTime = req.session.data['first-time']
+    
+    if (firstTime === 'Yes') {
+        res.redirect('/pe-allocation/request-sent-3')
+    } else {
+        res.redirect('/pe-allocation/no-request-confirmed-3')
+    }
+})
+
 router.post('/pe-allocation/new-provider', function (req, res) {
     // Get the answer from session data
     // The name between the quotes is the same as the 'name' attribute on the input elements
