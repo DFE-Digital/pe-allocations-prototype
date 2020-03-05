d20f8c2476d4a10ada8173e9adc30a61f4c2f9ff
Jesse Yuen <hello@jesseyuen.com>
2020-03-05 14:33:51 +0000

    tidy up

--- app/routes.js
+++ app/routes.js
@@ -59,18 +59,4 @@ router.post('/pe-allocation/new-provider', function (req, res) {
     }
 })
 
-// router.post('/pe-allocation/another-course', function (req, res) {
-//     // Get the answer from session data
-//     // The name between the quotes is the same as the 'name' attribute on the input elements
-//     // However in JavaScript we can't use hyphens in variable names
-    
-//     let firstTime = req.session.data['another-course']
-    
-//     if (firstTime === 'Yes') {
-//         res.redirect('/pe-allocation/next-entry')
-//     } else {
-//         res.redirect('/pe-allocation/submit-answers')
-//     }
-// })
-
 module.exports = router
