ec6b9a1a11429138ae254dced93f24ee34cb5529
Jesse Yuen <hello@jesseyuen.com>
2020-04-14 13:53:28 +0100

    iteration 3 added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,35 @@ if (window.console && window.console.info) {
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
+
+// ;(function (global) {
+//   'use strict'
+
+//   var GOVUK = global.GOVUK || {}
+//   GOVUK.Modules = GOVUK.Modules || {}
+
+//   GOVUK.Modules.Edge = function () {
+//     this.start = function (element) {
+//       element.on('click', 'a[href="#"], .js-edge', alertUser);
+
+//       function alertUser(evt) {
+//         evt.preventDefault();
+//         var target = $(evt.target);
+//         var message = target.data('message') || 'Sorry, this hasn’t been built yet'
+
+//         alert(message);
+//       }
+//     }
+//   }
+
+//   global.GOVUK = GOVUK
+// })(window)
+
+$('a[href*=\\#]').on('click', function (event) {
+  if(this.pathname === window.location.pathname){
+    
+    var message ='Sorry, this hasn’t been built yet';
+    alert(message);
+
+  }
+});
